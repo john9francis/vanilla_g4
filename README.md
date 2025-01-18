026c503d31998ebee66ba2ea22571b7c7ef7ddd4
John Francis <john9francis@gmail.com>
2025-01-17 21:05:21 -0500

    Set up the main file and it's not working yet because issues with my Geant4 installation. Time to move over to docker

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # Vanilla G4
 
 A general purpose Geant4 app using the most vanilla Geant4 possible.
+
+# How to compile
+```sh
+mkdir build
+cd build
+cmake ..
+make # (or build with VS)
+```
--- src/main.cc
+++ src/main.cc
@@ -1,5 +1,12 @@
-#include "G4UImanager.hh"
+#include "G4UIterminal.hh"
 
-int main(){
+int main(int argc, char** argv){
+
+  // init ui stuff
+  G4UIsession* ui = new G4UIterminal();
+
+  ui->SessionStart();
+  delete ui;
+
   return 0;
 }
